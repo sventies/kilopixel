381e4f5271754f82441462a897732072df409104
Sven <8081258+sventies@users.noreply.github.com>
2024-11-27 21:15:54 +0100

    add jazz

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createJazzReactApp, useDemoAuth, DemoAuthBasicUI } from "jazz-react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const Jazz = createJazzReactApp();
+export const { useAccount, useCoState } = Jazz;
+function JazzAndAuth({ children }: { children: React.ReactNode }) {
+  const [auth, authState] = useDemoAuth();
+  return (
+    <>
+      <Jazz.Provider
+        auth={auth}
+        // replace `you@example.com` with your email as a temporary API key
+        peer="wss://cloud.jazz.tools/?key=you@example.com"
+      >
+        {children}
+      </Jazz.Provider>
+      {authState.state === "signedIn" ? (
+        <button
+          onClick={() => {
+            authState.logOut();
+            window.location.reload();
+          }}
+        >
+          Sign out
+        </button>
+      ) : (
+        <DemoAuthBasicUI appName="Circular" state={authState} />
+      )}
+    </>
+  );
+}
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <JazzAndAuth>
+      <App />
+    </JazzAndAuth>
+  </React.StrictMode>
+);
